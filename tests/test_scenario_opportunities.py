5667279bb6c34010f8226f7db93dceac117d095a
Connor Griffin <connorgriffin519@gmail.com>
2026-09-10 07:04:08 -0700

    Regenerate sequence outputs and extend scenario test settings stub

--- tests/test_scenario_opportunities.py
+++ tests/test_scenario_opportunities.py
@@ -44,6 +44,10 @@ def test_mutating_the_shared_builder_changes_both_consumers(monkeypatch):
 
     class StoreStub:
         @staticmethod
+        def settings_snapshots():
+            return []
+
+        @staticmethod
         def prompt_responses():
             return []
 
